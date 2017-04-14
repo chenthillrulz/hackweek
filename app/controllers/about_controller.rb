03588c42c10016031ecccbd94231cd37d3ea3622
Chenthill Palanisamy <chenthill.palanisamy@softwareag.com>
2017-04-14 14:37:23 +0530

    Allow home page image to be shown without padding.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,14 @@
 class AboutController < ApplicationController
   skip_before_filter :authenticate_user!, only: [ :show, :index ]
+  before_filter :set_home_page, only: [:index]
 
   def index; end
   def show; end
+
+  protected
+
+  def set_home_page
+    @is_home_page = true
+  end
+
 end
