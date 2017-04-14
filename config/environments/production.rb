142fa2a172c2274d557d0cfb98ad78af8874d1a6
Chenthill Palanisamy <chenthill.palanisamy@softwareag.com>
2017-04-14 18:48:39 +0530

    precompile fonts in production mode.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Hackweek::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
